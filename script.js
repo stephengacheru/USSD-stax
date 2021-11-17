9e6e17762ce5aebd2edfd5cd37f00738f5f0ee41
kutalek <davkutalek@gmail.com>
2021-11-17 13:58:09 -0800

    Show error messages, sort channels by name, minor cleanup

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
 const root_url = "http://localhost:3000/";
 let countries = [], country = null, channels = [];
 
-function load(url, callback) { $.ajax({type: "GET", url: url, success: callback, error: function(result) { onError(result); } }); }
+function load(url, callback, errorCallback) { $.ajax({type: "GET", url: url, success: callback, errorCallback }); }
 
 function loadCountries() {
-	load(root_url + "api/countries?channels=true", onLoadCountries); 
+	load(root_url + "api/countries?channels=true", onLoadCountries, countriesError); 
 }
 
 function loadChannels() { 
 	const urlParams = new URLSearchParams(window.location.search);
 	if (urlParams.get("country")) {
-		country = countries.find(c => { console.log(c.alpha2.toUpperCase()); return c.alpha2.toUpperCase() === urlParams.get("country").toUpperCase(); });
+		country = countries.find(c => { return c.alpha2.toUpperCase() === urlParams.get("country").toUpperCase(); });
 	}
 
-	var url = root_url + "api/channels?bookmarked=true";
+	var url = root_url + "api/channels?bookmarked=true&order_key=name";
 	if (country) {
 		$("#selected-country-dropdown").text(getCountryFlag(country) + " " + country.name.toUpperCase());
 		url += "&country=" + country.alpha2;
 	}
-	load(url, onLoadChannels); 
+	load(url, onLoadChannels, channelsError); 
 }
 
 function fillInDropdowns() { 
-	countries.forEach(country => {
-		var link = document.createElement("a");
-		link.className = "dropdown-item " + country.alpha2;
-		link.href = "ussd-codes.html?country=" + country.alpha2;
-		link.innerHTML = getCountryFlag(country) + " " + country.name;
-		var li = document.createElement("li");
-		li.append(link);
-		$("#dropdown-country-list").append(li.cloneNode(true));
-		$("#header-dropdown-country-list").append(li);
-	});
-	
+	countries.forEach(country => addCountry(country));
+}
+
+function addCountry(country) {
+	var link = document.createElement("a");
+	link.className = "dropdown-item " + country.alpha2;
+	link.href = "ussd-codes.html?country=" + country.alpha2;
+	link.innerHTML = getCountryFlag(country) + " " + country.name;
+	var li = document.createElement("li");
+	li.append(link);
+	$("#dropdown-country-list").append(li.cloneNode(true));
+	$("#header-dropdown-country-list").append(li);
 }
 
 function fillInList() { 
 	$("#channels-loading").hide();
-	channels.forEach(channel => {
-		var link = document.createElement("a");
-		link.href = "tel:" + channel.root_code.replace("#", "%23");
-		link.innerHTML = channel.root_code;
-		var li = document.createElement("li");
-		li.append(link, getChannelName(channel));
-		$("#channel-list").append(li);
-	});
-	
+	channels.forEach(channel => addChannel(channel));
 }
+
+function addChannel(channel) {
+	var link = document.createElement("a");
+	link.href = "tel:" + channel.root_code.replace("#", "%23");
+	link.innerHTML = channel.root_code;
+	var li = document.createElement("li");
+	li.append(link, getChannelName(channel));
+	$("#channel-list").append(li);
+}
+
 
 function getChannelName(channel) {
 	var name = " " + channel.name;
@@ -60,22 +63,23 @@ function getCountryFlag(country) {
 	return String.fromCodePoint(...codePoints);
 }
 
-function onLoadCountries(result) { 
-	console.log(result);
+function onLoadCountries(result) {
 	countries = result.map(function(d) { return d; });
 	loadChannels();
 	fillInDropdowns();
 }
 
-function onLoadChannels(result) { 
-	console.log(result);
+function onLoadChannels(result) {
 	channels = result.data.map(function(d) { return d.attributes; });
 	fillInList();
 }
 
-function onError(result) {
-	console.log(result.body);
-	$("#dropdown-country-label").text(result);
+function countriesError() {
+	$("#dropdown-country-label").text("Network error, please reload.");
+}
+
+function channelsError() {
+	$("#channels-loading").text("Network error, please reload.")
 }
 
 loadCountries();
